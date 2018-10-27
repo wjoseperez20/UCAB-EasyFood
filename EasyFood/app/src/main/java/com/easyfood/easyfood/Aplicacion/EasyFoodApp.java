d069d331249548cb6a93a70d3dde818d2afc8c91
FelixMorales <felix_morales_@hotmail.com>
2018-10-26 20:25:38 -0400

    Revert "Revert "Creando Modelo de clases y estructura base del aplicativo.""
    This reverts commit 2a6024bb3f40193dc8ffc4ecdeb6bf4f56aa3109.

---
+++ EasyFood/app/src/main/java/com/easyfood/easyfood/Aplicacion/EasyFoodApp.java
@@ -0,0 +1,18 @@
+package com.easyfood.easyfood.Aplicacion;
+
+import android.app.Application;
+
+import com.easyfood.easyfood.Logica.EasyFood;
+
+public class EasyFoodApp extends Application {
+
+    private static EasyFood _easyFood;
+
+    public EasyFoodApp(){
+        _easyFood = new EasyFood();
+    }
+
+    public static EasyFood getEasyFood(){
+        return  _easyFood;
+    }
+}
